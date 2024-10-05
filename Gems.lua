7a44adc0fc19a06821685f011fb5e3eb851387ea
NUTTOBELT <nutnewidsad@gmail.com>
2024-10-05 11:51:02 +0700

    Update Gems.lua

--- Gems.lua
+++ Gems.lua
@@ -1,12 +1,3 @@
 game.Players.LocalPlayer.Stats.Gems.Value=25000
-game.Players.LocalPlayer.Stats.Gems.Value=25000
-game.Players.LocalPlayer.Stats.Gems.Value=25000
-game.Players.LocalPlayer.Stats.Gems.Value=25000
 game:GetService("ReplicatedStorage").SaveGems:FireServer(25000)
-game:GetService("ReplicatedStorage").SaveGems:FireServer(25000)
-game:GetService("ReplicatedStorage").SaveGems:FireServer(25000)
-game:GetService("ReplicatedStorage").SaveGems:FireServer(25000)
-game:GetService("ReplicatedStorage").ExploitingGems:FireServer(25000)
-game:GetService("ReplicatedStorage").ExploitingGems:FireServer(25000)
-game:GetService("ReplicatedStorage").ExploitingGems:FireServer(25000)
 game:GetService("ReplicatedStorage").ExploitingGems:FireServer(25000)
